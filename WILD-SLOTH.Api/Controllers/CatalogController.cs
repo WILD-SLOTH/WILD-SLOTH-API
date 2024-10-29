3136e1edfd20afc0d564122485293a22991d7d73
JMatt137 <divejj@outlook.com>
2024-10-29 19:14:28 -0400

    Item Rating and Controllers

---
+++ WILD-SLOTH.Api/Controllers/CatalogController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using WILD.SLOTH.Domain.catalog;
+
+namespace WILD.SLOTH.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+
+    }
+}
---
+++ WILD-SLOTH.Domain/catalog/Item.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics.Contracts;
+
+
+namespace WILD.SLOTH.Domain.catalog
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public Item(string name, string description, string brand, decimal price)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(name);
+            }
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentNullException(description);
+            }
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentNullException(brand);
+            }
+            if (price < 0.00m)
+            {
+                throw new ArgumentException("Price must be greater than zero.");
+            }
+            Name = name;
+            Description = description;
+            Brand = brand;
+            Price = price;
+        }
+        public List<Rating> Ratings { get; set; } = new List<Rating>();
+        public void AddRating(Rating rating)
+        {
+            this.Ratings.Add(rating);
+        }
+    }
+}
