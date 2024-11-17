f6f922e806d594199e269731a52d0c2fed233eac
JMatt137 <divejj@outlook.com>
2024-11-17 18:44:36 -0500

    new catalog controller

--- WILD-SLOTH.Api/Controllers/CatalogController.cs
+++ WILD-SLOTH.Api/Controllers/CatalogController.cs
@@ -1,98 +1,109 @@
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
-using WILD.SLOTH.Data;
 using WILD.SLOTH.Domain.catalog;
+using WILD.SLOTH.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WILD.SLOTH.Api.Controllers
 {
-    [EnableCors]
     [ApiController]
-    [Route("/catalog")]
+    [Route("[controller]")]
     public class CatalogController : ControllerBase
     {
-        private readonly StoreContext _db;
 
-        public CatalogController(StoreContext db)
-        {
-            _db = db;
-        }
+        // private readonly StoreContext _db;
+
+        // public CatalogController(StoreContext db)
+        // {
+        //     _db = db;
+        // }
 
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok(_db.Items);
-            
-          /*  var items = new List<Item>()
+            //return Ok("Hello World--");
+            var items = new List<Item>()
             {
-                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+                new Item("Shirt", "Ohio State Shirt", "Nike", 39.99m),
+                new Item("Shorts", "Ohio State shorts", "Nike", 49.99m)
             };
-            return Ok(items); */
+            return Ok(items);
+            //return Ok(_db.Items);
         }
+
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id)
         {
-            var item = _db.Items.Find(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
-            return Ok();
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 59.99m);
+            item.Id = id;
+
+            // var item = _db.Items.Find(id);
+            // if (item == null)
+            // {
+            //     return NotFound();
+            // }
+            return Ok(item);
         }
+
         [HttpPost]
-        public IActionResult Post(Item item)
+        public IActionResult CreateItem(Item item)
         {
-            _db.Items.Add(item);
-            _db.SaveChanges();
-            return Created($"/catalog/{item.Id}", item);
+            // return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+            return Created("/catalog/42", item);
+            // _db.Items.Add(item);
+            // _db.SaveChanges();
+            // return Created($"/catalog/{item.Id}", item);
         }
+
         [HttpPost("{id:int}/ratings")]
         public IActionResult PostRating(int id, [FromBody] Rating rating)
         {
-            var item = _db.Items.Find(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
+            // var item = _db.Items.Find(id);
+            // if (item == null)
+            // {
+            //     return NotFound();
+            // }
+            // item.AddRating(rating);
+            // _db.SaveChanges();
 
-            item.AddRating(rating);
-            _db.SaveChanges();
-
-            return Ok(item);
+            // return Ok(item);
+            return Ok();
         }
+
         [HttpPut("{id:int}")]
-        public IActionResult PutItem(int id, [FromBody] Item item)
+        public IActionResult Put(int id, Item item)
         {
-            if (id != item.Id)
-            {
-                return BadRequest();
-            }
+            // if (id != item.Id)
+            // {
+            //     return BadRequest();
+            // }
 
-            if (_db.Items.Find(id) == null)
-            {
-                return NotFound();
-            }
+            // if (_db.Items.Find(id) == null)
+            // {
+            //     return NotFound();
+            // }
 
-            _db.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _db.SaveChanges();
+            // _db.Entry(item).State = EntityState.Modified;
+            // _db.SaveChanges();
 
             return NoContent();
         }
-        [HttpDelete("{id:int}")]
-        public IActionResult DeleteItem(int id)
-        {
-            var item = _db.Items.Find(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
 
-            _db.Items.Remove(item);
-            _db.SaveChanges();
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            // var item = _db.Items.Find(id);
+            // if (item == null)
+            // {
+            //     return NotFound();
+            // }
+
+            // _db.Items.Remove(item);
+            // _db.SaveChanges();
 
             return Ok();
         }
+
     }
+
 }
+
